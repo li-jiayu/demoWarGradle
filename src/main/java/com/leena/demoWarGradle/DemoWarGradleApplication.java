64268843f34b8e3dddd44b3d3043b0e7c54ba809
Philip <A147@ad.moneygram.com>
2019-11-14 11:13:08 -0600

    first commit

---
+++ src/main/java/com/leena/demoWarGradle/DemoWarGradleApplication.java
@@ -0,0 +1,13 @@
+package com.leena.demoWarGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leena/demoWarGradle/DemoWarGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leena.demoWarGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWarGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
