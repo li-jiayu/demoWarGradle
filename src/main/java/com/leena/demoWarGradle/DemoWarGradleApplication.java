92f10eb435bf1811f56c0dfd2dc81fded229844c
jiayu.li <jiayu.li@openjawtech.com>
2020-01-14 18:21:07 +0800

    JIRA:  test Description:  test

--- src/main/java/com/leena/demoWarGradle/DemoWarGradleApplication.java
+++ src/main/java/com/leena/demoWarGradle/DemoWarGradleApplication.java
@@ -2,12 +2,18 @@ package com.leena.demoWarGradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoWarGradleApplication {
+public class DemoWarGradleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWarGradleApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoWarGradleApplication.class);
+	}
 }
